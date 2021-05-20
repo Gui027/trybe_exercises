f076d5d861452e2d7112d72b42d324b63deb22eb
Guilherme <guilhermerg1234@gmail.com>
2021-05-19 22:06:40 -0300

    Adicionar ex 5

---
+++ bloco_4/dia_1/ex5/scripts.js
@@ -0,0 +1,12 @@
+let a = -8;
+let b = 60;
+let c = 60;
+let soma = (a + b + c);
+
+if (soma < 0) {
+    console.log("ERRO")
+} else if (soma > 0 && soma == 180) {
+    console.log("TRUE")
+} else if (soma > 0 && soma != 180) {
+    console.log("FALSE")
+}
