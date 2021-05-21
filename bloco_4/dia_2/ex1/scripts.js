bd834a82360e3352efd4758b959b3cbe60d9af37
Guilherme <guilhermerg1234@gmail.com>
2021-05-21 17:39:42 -0300

    voltando branch

---
+++ bloco_4/dia_2/ex1/scripts.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index])
+}
+
+// Algorítimo
+
+/* 1º - Escrever o array.
+    2º - Criar um for para passar sobre todos os elementos.
+    3ª - Colocar um console.log no bloco de cód do for. */
