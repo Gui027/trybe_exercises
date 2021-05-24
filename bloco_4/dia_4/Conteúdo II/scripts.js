298ef3707f87e11d98fbb93be4067f777caf92a7
Guilherme <guilhermerg1234@gmail.com>
2021-05-24 19:04:09 -0300

    Adicionando Conteudo II

--- bloco_4/dia_4/Conteúdo II/scripts.js
+++ bloco_4/dia_4/Conteúdo II/scripts.js
@@ -1,3 +1,5 @@
+//Ex1
+
 let names = {
     person1: 'João',
     person2: 'Maria',
@@ -6,4 +8,16 @@ let names = {
 
   for (let ola in names) {
     console.log('Olá ' + names[ola]);
+  }
+
+//Ex2
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let key in car) {
+    console.log(key, car[key])
   }
