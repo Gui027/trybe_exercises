e230541b601f7235079c365342ea328cee31c572
Guilherme <guilhermerg1234@gmail.com>
2021-05-25 23:33:43 -0300

    Adicionando ex3

---
+++ bloco_4/dia_3/ex3/scripts.js
@@ -0,0 +1,18 @@
+let size = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = size;
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex++) {
+    for(let columnIndex = 0; columnIndex <= size; columnIndex += 1) {
+        if (columnIndex < inputPosition) {
+            inputLine = inputLine + ' ';
+        } else {
+            inputLine = inputLine + symbol;
+        }
+    }
+    console.log(inputLine);
+    inputLine = '';
+    inputPosition -= 1;
+}
+
