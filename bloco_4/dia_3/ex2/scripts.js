55730c5ed1972f0460245dd422dc7dc7cd98584e
Guilherme <guilhermerg1234@gmail.com>
2021-05-25 23:27:02 -0300

    Adicionando ex2

---
+++ bloco_4/dia_3/ex2/scripts.js
@@ -0,0 +1,8 @@
+let size = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex++) {
+    console.log(inputLine);
+    inputLine = inputLine + symbol;
+}
