15d7d335dbaace548edf6e610c2d9ca22756d0d1
Guilherme <guilhermerg1234@gmail.com>
2021-05-25 23:11:06 -0300

    Adicionando ex1

---
+++ bloco_4/dia_3/ex1/scripts.js
@@ -0,0 +1,12 @@
+let line = 5;
+let symbol = '*';
+let inputLine = '';
+
+
+for(let index = 0; index < line; index += 1) {
+    inputLine = inputLine + symbol;
+}
+
+for (let index = 0; index < line; index++) {
+    console.log(inputLine);
+}
