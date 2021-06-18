22b3e749d02b1ec980a19651e636af7353bb5454
Guilherme <guilhermerg1234@gmail.com>
2021-06-17 22:58:26 -0300

    Part2Ex1

--- bloco_7/dia_1/Ex1/script.js
+++ bloco_7/dia_1/Ex1/script.js
@@ -24,8 +24,17 @@ console.log(oddsAndEvens);
 
 //Parte2
 //Ex1
+const Fatorial = number => {
+  let resultado = 1;
+  for (let index = 2; index <= number; index++){
+    resultado *= index;
+  }
+  return resultado;
+}
+console.log(Fatorial(5));
 
-const factorial = number => number > 1 ? number * factorial(number-1) : 'Erro'
+
+const factorial = number => number > 1 ? number * factorial(number-1) : 1
 console.log(factorial(5));
 
 //Ex2
