4f643ed0221b566e3b4c5fbddc0efc2b65e84fec
Guilherme <guilhermerg1234@gmail.com>
2021-06-25 19:36:15 -0300

    Ex2

--- bloco_8/dia_3/Ex/script.js
+++ bloco_8/dia_3/Ex/script.js
@@ -80,3 +80,42 @@ const expectedResult = [
   }
   console.log(formatedBookNames());
   assert.deepStrictEqual(formatedBookNames(), expectedResult);
+
+//Ex2
+const expectedResult = [
+    {
+      age: 31,
+      author: 'Isaac Asimov',
+    },
+    {
+      age: 38,
+      author: 'H. P. Lovecraft',
+    },
+    {
+      age: 39,
+      author: 'Stephen King',
+    },
+    {
+      age: 43,
+      author: 'George R. R. Martin',
+    },
+    {
+      age: 45,
+      author: 'Frank Herbert',
+    },
+    {
+      age: 62,
+      author: 'J. R. R. Tolkien',
+    },
+  ];
+  
+  const nameAndAge = () => {
+    return books.map((book) => (
+        {
+        age: book.releaseYear - book.author.birthYear,
+        author: book.author.name,
+        }
+    )).sort((a, b) => a.age - b.age)
+  }
+  console.log(nameAndAge());
+  assert.deepStrictEqual(nameAndAge(), expectedResult);
