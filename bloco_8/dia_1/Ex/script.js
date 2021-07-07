da5eaae05e5f4624e546a3551f16c7c0def09066
Guilherme <guilhermerg1234@gmail.com>
2021-07-06 21:10:35 -0300

    Ex

--- bloco_8/dia_1/Ex/script.js
+++ bloco_8/dia_1/Ex/script.js
@@ -1,12 +1,16 @@
 //1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
-const newEmployees = () => {
+const newEmployees = (novoFuncionario) => {
     const employees = {
-      id1: 'Pedro Guerra', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: 'Luiza Drumond', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: 'Carla Paiva', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: novoFuncionario('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: novoFuncionario('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: novoFuncionario('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
 
-  const qualquer = (param)
+  const novoFuncionario = (nomeCompleto) => {
+    const email = nomeCompleto.toLowerCase().split(' ').join('_');
+    return {nomeCompleto, email: `${email}@trybe.com`}
+  }
+  console.log(newEmployees(novoFuncionario));
---
+++ bloco_8/dia_1/Ex/script2.js
@@ -0,0 +1,10 @@
+  // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+  const sorteio = (numeroSorteado, numeroEscolhido) => numeroSorteado === numeroEscolhido ? 'Parabéns vocÊ ganhou' : 'Tente novamente';
+
+  const randomNum = (min, max) => {
+      min= Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  console.log(sorteio(2, randomNum(1, 5)));
